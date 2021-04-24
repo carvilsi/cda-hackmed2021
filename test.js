6f00bfb4e64b18f21096bac5e73e620b42509870
carvilsi <carvilsi.mail.list@gmail.com>
2021-04-24 20:08:52 +0200

    templating xml

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
+const fs = require('fs')
+var Mustache = require('mustache');
 
 function handleNewButton() {
-  console.log("🐛 BUTTON")
+  try {
+    console.log(JSON.stringify($("#myForm").serializeArray()));
+    var data = $("#myForm").serializeArray();
+    toTemplate = {}
+    data.forEach((object, i) => {
+      toTemplate[object[Object.keys(object)[0]]] = object.value
+    });
 
+    fs.readFile('./template.mustache', function (err, data) {
+      if (err) throw err;
+      var output = Mustache.render(data.toString(), toTemplate);
+      console.log(`🐛 ${JSON.stringify(output)}`)
+    });
+
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-
 onload = function() {
-  console.log("🐛 !!!")
-
   newButton = document.getElementById("button");
 
   newButton.addEventListener("click", handleNewButton);
