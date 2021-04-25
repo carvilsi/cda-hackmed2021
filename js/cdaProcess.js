48f7b9c7085c41b5f3ab4e4ef5e300ad2928dcfd
Luis Alario Francés <lalario@kanteron.com>
2021-04-25 21:05:23 +0200

    Including patient’s name list WIP
    The next step is to get the mustache result and put into the general JSON

--- js/cdaProcess.js
+++ js/cdaProcess.js
@@ -5,6 +5,33 @@ var dialog     = remote.dialog;
 
 async function handleNewButton() {
   try {
+    console.log(JSON.stringify($("#patientNameForm").serializeArray()));
+    var patientNamesJsonArray = serializedMultipleForm2JsonArrayGroups($("#patientNameForm").serializeArray(), "family");
+    const patientNamesTemplate = await fs.readFileSync('./templates/patientNameTemplate.mustache');
+    var patientNamesOutput = "";
+    patientNamesJsonArray.forEach((nameEntry,j) => {
+        toNamesTemplate = {}
+        nameEntry.forEach((object, i) => {
+          toNamesTemplate[object[Object.keys(object)[0]]] = object.value
+
+          var jsonObject;
+          try {
+            jsonObject = JSON.parse(object.value);
+          } catch (exc) {
+            console.log ("it is not JASON");
+          }
+
+          if (typeof jsonObject !== 'undefined') {
+            $.each(jsonObject, function(objectName, objectValue) {
+              toNamesTemplate[object[Object.keys(object)[0]] + '__' + objectName] = objectValue;
+            });
+          }
+        });
+        patientNamesOutput += Mustache.render(patientNamesTemplate.toString(), toNamesTemplate) + "\n";
+    });
+    console.log(patientNamesOutput);
+    console.log(JSON.stringify($("#languagesForm").serializeArray()));
+    var patientLanguagesJsonArray = serializedMultipleForm2JsonArrayGroups($("#languagesForm").serializeArray(), "languageCode");
     console.log(JSON.stringify($("#reportForm").serializeArray()));
     var data = $("#reportForm").serializeArray();
 
@@ -57,4 +84,26 @@ onload = function() {
   newButton.addEventListener("click", handleNewButton);
 
 
+};
+
+serializedMultipleForm2JsonArrayGroups = function(serializedForm, initialField) {
+    totalArray = [];
+    currentArray = [];
+    serializedForm.forEach((object, i) => {
+        if ((object[Object.keys(object)[0]] === initialField) 
+                && (currentArray.length)) {
+            totalArray.push(currentArray);
+            currentArray = [];
+        }
+        currentArray.push(object);
+    });
+    
+    if (currentArray.length) {
+        totalArray.push(currentArray);
+    }
+
+    console.log (totalArray);
+    
+    return totalArray;
+  
 };
